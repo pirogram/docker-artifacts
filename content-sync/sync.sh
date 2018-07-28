8de58c77d76867b748f25c1c16428437cb149f24
Manas Garg <manasgarg@gmail.com>
2018-07-28 14:10:45 -0700

    add ms to time format

--- content-sync/sync.sh
+++ content-sync/sync.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rsync -avz /content/src/ /content/dst
+rsync -avz /content/src/ /content/dst --delete
 
 while inotifywait -r -e modify,create,delete /content/src; do rsync -avz /content/src/ /content/dst --delete; done
