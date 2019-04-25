505e9adb9b8b84eef02761980bc3835ed2d55a8e
Manas Garg <manasgarg@gmail.com>
2019-04-24 19:44:37 -0700

    updated nginx and pi_serv

--- pirogram-server/refresh-books.sh
+++ pirogram-server/refresh-books.sh
@@ -4,4 +4,4 @@ cd /tmp/
 rm -rf pirogram-content-master
 curl -L -o pirogram-content.zip https://github.com/pirogram/pirogram-content/archive/master.zip 
 unzip pirogram-content.zip
-rsync -avz pirogram-content-master/* /mnt/pirogram-content
+rsync -avz --delete pirogram-content-master/* /mnt/pirogram-content
